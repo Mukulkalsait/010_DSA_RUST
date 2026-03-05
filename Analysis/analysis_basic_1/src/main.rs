f1abec984afc581397299de7116aa2c4680c1712
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-03-05 23:39:40 +0530

    updates

--- Analysis/analysis_basic_1/src/main.rs
+++ Analysis/analysis_basic_1/src/main.rs
@@ -1,18 +1,40 @@
 
+mod modules;
 mod linear;
 mod nested;
 mod logarithmic;
-mod nlog;
+mod nlogn;
+mod game;
+
 
 fn main() {
+    println!("Hello and welcome to DSA in rust - learning...");
+
+    // let mut input= String::from("");
+
+    // select_what_to_learn();
+
+    game::game::game_main();
+
+}
+
+fn select_what_to_learn(){
+
+
     // println!("----------| Linear |----------");
     // linear::run_linear();
     //
     // println!("----------| Nested |----------");
     // nested::run_nested();
+    //
+    // println!("----------| Logarithmic |----------");
+    // logarithmic::run_logarithmic();
 
-    println!("----------| Logarithmic |----------");
-    logarithmic::run_logarithmic();
+    // println!("----------| N Log N  |----------");
+    // nlogn::run_n_log_n();
+    
+    println!("----------|  matrix_sum  |----------");
+    // modules::matrix_sum::matrics_sum_main();
 
-    println!("----------| N Log N  |----------");
 }
+
